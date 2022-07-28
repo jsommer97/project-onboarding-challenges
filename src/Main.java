8e65fa4aa6786c227b6dbb6f4a32f4e041e79668
Jasper Sommer <jasper.sommer97@gmail.com>
2022-07-28 15:21:54 -0400

    coding challenge

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,27 @@
-import models.LinkedList;
-import models.Node;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        Node five = new Node(5);
-        Node four = new Node(4, five);
-        Node three = new Node(3, four);
-        Node two = new Node(2, three);
-        Node one = new Node(1, two);
+        System.out.println(largestGap(new int[]{9, 4, 26, 26, 0, 0, 5, 20, 6, 25, 5}));
+        System.out.println(largestGap(new int[]{14, 13, 7, 1, 4, 12, 3, 7, 7, 12, 11, 5, 7}));
+        System.out.println(largestGap(new int[]{13, 3, 8, 5, 5, 2, 13, 6, 14, 2, 11, 4, 10, 8, 1, 9}));
 
-        LinkedList list1 = new LinkedList(one);
-
-        Node ten = four;
-        Node nine = new Node(9, ten);
-        Node eight = new Node(8, nine);
-        Node seven = new Node(7, eight);
-        Node six = new Node(6, seven);
-
-        LinkedList list2 = new LinkedList(six);
-        System.out.println("Intersection: ");
-        System.out.println(intersect(list1, list2).toString());
-
-        LinkedList loopedList = list1.insert(list1, three);
-        System.out.println("Loop: ");
-        System.out.println(loopDetection(loopedList).toString());
     }
 
-    public static Node intersect(LinkedList list1, LinkedList list2) {
-        Node pointer = list1.head;
-        Node check = list2.head;
+    public static int largestGap(int[] array) {
+        // sort array if not already sorted
+        Arrays.sort(array);
+        int gap = 0;
 
-        while(pointer != null) {
+        for(int i = 1; i < array.length; i++) {
+            int g = array[i] - array[i-1];
 
-            while(check != null) {
-                if(pointer.equals(check)) {
-                    return check;
-                }
-                check = check.next;
+            if (g > gap) {
+                gap = g;
             }
-            check = list2.head;
-            pointer = pointer.next;
         }
-        return null;
-    }
-
-    public static Node loopDetection(LinkedList list) {
-
-        ArrayList<Node> foundNodes = new ArrayList<>();
-        Node pointer = list.head;
-
-        while(pointer != null) {
-            if (foundNodes.contains(pointer)) {
-                return pointer;
-            }
-
-            foundNodes.add(pointer);
-            pointer = pointer.next;
-        }
-        return null;
+        return gap;
     }
 }
+
